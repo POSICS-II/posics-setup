c3cddab0df01cacb9062c5ab2cd69cfd39c44f1b
Cyril Alispach <cyril23462345+calispac@users.noreply.github.com>
2022-07-13 09:57:50 +0200

    define new center

--- move_linear_stage.py
+++ move_linear_stage.py
@@ -1,15 +1,18 @@
 from posicssetup.instruments.linear_stage import LinearStageTDC001
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
 
 config_file = 'config.json'
 
 stage_vertical = LinearStageTDC001(config_file=config_file, name='vertical')
 stage_horizontal = LinearStageTDC001(config_file=config_file, name='horizontal')
 
-y_position = 20.32
-x_position = 27.09
+x_position = 30.8
+y_position = 21.82
 
-stage_vertical.move_to(x_position)
-stage_horizontal.move_to(y_position)
+stage_horizontal.move_to(x_position)
+stage_vertical.move_to(y_position)
 
 
 
