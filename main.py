eb9b09ec6bc407f2ccc480b962450f801a9df35f
Cyril Alispach <cyril23462345+calispac@users.noreply.github.com>
2022-07-19 16:42:28 +0200

    update main script and center position

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
-import logging
 import matplotlib
-import h5py
 
 from posicssetup.instruments.oscilloscopes import PicoScope6000
 from posicssetup.instruments.pulse_generator import PulseGeneratorTG5011
 from posicssetup.instruments.linear_stage import LinearStageTDC001
 from posicssetup.instruments.power_supply import Keithley2400
 from posicssetup.io.writer import HDF5FileWriter
+from posicssetup.io.utils import get_basename
+from posicssetup.io.logger import Logger
 
 
 matplotlib.use('TkAgg')
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
-logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
+#logger = logging.getLogger(__name__)
 
 config_file = 'config.json'
 
+basename = get_basename(config_file)
+logger = Logger(config_file=config_file, basename=basename)
 
-writer = HDF5FileWriter(config_file=config_file)
+writer = HDF5FileWriter(config_file=config_file, basename=basename)
 picoamp = Keithley2400(config_file=config_file)
 picoamp.set_output(True)
 
@@ -36,10 +38,10 @@ pulse_generator.set_main_out(True)
 pulse_generator.set_synch_out(False)
 
 ps_1 = PicoScope6000(config_file=config_file, serial='FW878/020')
-ps_2 = PicoScope6000(config_file=config_file, serial='FW881/048')
+# ps_2 = PicoScope6000(config_file=config_file, serial='FW881/048')
 
 ps_1.check_status()
-ps_2.check_status()
+# ps_2.check_status()
 
 start_horizontal = stage_horizontal.center + stage_horizontal.length / 2
 start_vertical = stage_vertical.center + stage_vertical.length / 2
@@ -49,6 +51,9 @@ stage_vertical.move_to(start_vertical)
 
 x_previous = start_horizontal
 y_previous = start_vertical
+
+fig = plt.figure()
+axes = fig.add_subplot()
 
 for i in tqdm(range(stage_horizontal.n_steps), desc='Horizontal', leave=False):
 
@@ -66,23 +71,42 @@ for i in tqdm(range(stage_horizontal.n_steps), desc='Horizontal', leave=False):
         y_previous = y
 
         ps_1._setup_trigger()
-        ps_2._setup_trigger()
+        # ps_2._setup_trigger()
         ps_1._setup_data()
-        ps_2._setup_data()
+        # ps_2._setup_data()
 
         time.sleep(2)
         pulse_generator.set_synch_out(True)
+        time.sleep(2)
 
         data_1 = ps_1.read_data()
-        data_2 = ps_2.read_data()
+        # data_2 = ps_2.read_data()
+        # times_1 = ps_1.read_trigger_times()
+        # times_2 = ps_2.read_trigger_times()
 
         ps_1.check_status()
-        ps_2.check_status()
+        # ps_2.check_status()
 
-        data = np.hstack([data_1, data_2])
+        # data = np.hstack([data_1, data_2])
+        # times = np.hstack([times_1, times_2])
+        data = data_1
+        # times = times_1
 
         pulse_generator.set_synch_out(False)
 
         writer.write_data(name="step_{}_{}".format(i, j), data=data)
+        # writer.write_data(name="step_{}_{}".format(i, j), data=times, group='trigger')
+
+        axes.clear()
+        axes.plot(ps_1.times * 1E9, np.mean(data, axis=0).T)
+        axes.set_xlabel('Time [ns]')
+        axes.set_ylabel('ADC []')
+        legend = ['Channel {}'.format(i) for i in range(data.shape[1])]
+        axes.legend(legend, loc='best')
+        fig.savefig(basename + '_waveforms_step_{}_{}.png'.format(i, j))
+
+        # plt.figure()
+        # plt.plot(times)
+        # plt.show()
 
 writer.close()
--- move_linear_stage.py
+++ move_linear_stage.py
@@ -8,11 +8,11 @@ config_file = 'config.json'
 stage_vertical = LinearStageTDC001(config_file=config_file, name='vertical')
 stage_horizontal = LinearStageTDC001(config_file=config_file, name='horizontal')
 
-x_position = 30.8
-y_position = 21.82
+x_position = 28.8
+y_position = 25.82
 
 stage_horizontal.move_to(x_position)
-stage_vertical.move_to(y_position)
+#stage_vertical.move_to(y_position)
 
 
 
